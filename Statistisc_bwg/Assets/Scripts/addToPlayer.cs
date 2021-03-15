01b6a58ae5008a202635b98a865675425282641c
JustaConfusedGuy <56404874+JustaConfusedGuy@users.noreply.github.com>
2021-03-15 18:59:08 +0100

    unfinnished product push do not install.

---
+++ Statistisc_bwg/Assets/Scripts/addToPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addToPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private SwitchScene identify;
+
+    public void IsAddKills(int playerInd)
+    {
+        playerInd = identify.playerIndex;
+        if (playerInd == 1)
+        {
+            Debug.LogWarning("Add kills to *playerName*: " + playerInd);
+            //It did not find the player index because the player index probably was not changed and saved
+        }
+    }
+}
